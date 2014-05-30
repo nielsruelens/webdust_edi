46a89b1d675d34f11f338af50ce7f6e16938cba8
Niels Ruelens <niels.ruelens@clubit.be>
2014-05-30 16:17:26 +0200

    added additional logging to schedulers

--- thr_masterdata_ftp_download.py
+++ thr_masterdata_ftp_download.py
@@ -1,6 +1,6 @@
 from openerp.osv import osv
 from openerp.tools.translate import _
-import ftplib
+import ftplib, logging
 from os.path import join
 
 
@@ -24,16 +24,21 @@ class product(osv.Model):
 
     def thr_ftp_download(self, cr, uid):
 
+        log = logging.getLogger(None)
+        log.info('THR_FTP: Starting the THR_FTP download process.')
+
         helpdesk_db = self.pool.get('crm.helpdesk')
         header = 'An error occurred during the THR_FTP download scheduler.'
 
         # Find the customizing
         # --------------------
+        log.info('THR_FTP: Searching for the THR_FTP connection settings.')
         flow_db = self.pool.get('clubit.tools.edi.flow')
         flow_id = flow_db.search(cr, uid, [('model', '=', 'product.product'),('method', '=', 'edi_import_availability')])[0]
         settings = self.pool.get('clubit.tools.settings').get_settings(cr, uid)
         ftp_info = [x for x in settings.connections if x.name == 'THR_FTP']
         if not ftp_info:
+            log.warning('THR_FTP: Could not find the THR_FTP connection settings, creating CRM helpdesk case.')
             helpdesk_db.create_simple_case(cr, uid, header, 'Missing THR_FTP connection in the EDI settings')
             return True
         ftp_info = ftp_info[0]
@@ -43,8 +48,10 @@ class product(osv.Model):
         # Connect to the FTP server
         # -------------------------
         try:
+            log.info('THR_FTP: Connecting to the FTP server.')
             ftp = ftplib.FTP(ftp_info.url, ftp_info.user, ftp_info.password)
         except Exception as e:
+            log.warning('THR_FTP: Could not connect to FTP server at {!s}, error given: {!s}'.format(ftp_info.url, str(e)))
             helpdesk_db.create_simple_case(cr, uid, header, 'Could not connect to FTP server at {!s}, error given: {!s}'.format(ftp_info.url, str(e)))
             return True
 
@@ -53,6 +60,7 @@ class product(osv.Model):
         # ---------------------------------------
         success = ftp.cwd('Artikelgegevens')
         if not success:
+            log.warning('THR_FTP: Connected to FTP server {!s}, but could not find folder Artikelgegevens'.format(ftp_info.url))
             helpdesk_db.create_simple_case(cr, uid, header, 'Connected to FTP server {!s}, but could not find folder Artikelgegevens'.format(ftp_info.url))
             ftp.quit()
             return True
@@ -60,8 +68,10 @@ class product(osv.Model):
 
         # Download the 2 files we need
         # ----------------------------
+        log.info('THR_FTP: Searching for files.')
         files = list_files(ftp)
         if not files:
+            log.warning('THR_FTP: Connected to FTP server {!s}, but folder Artikelgegevens did not contain any files'.format(ftp_info.url))
             helpdesk_db.create_simple_case(cr, uid, header, 'Connected to FTP server {!s}, but folder Artikelgegevens did not contain any files'.format(ftp_info.url))
             ftp.quit()
             return True
@@ -69,10 +79,12 @@ class product(osv.Model):
         # We're looking for CSV files
         files = [f for f in files if f[-1][-3:] == 'csv']
         if not files:
+            log.warning('THR_FTP: Connected to FTP server {!s}, but folder Artikelgegevens did not contain any csv files'.format(ftp_info.url))
             helpdesk_db.create_simple_case(cr, uid, header, 'Connected to FTP server {!s}, but folder Artikelgegevens did not contain any csv files'.format(ftp_info.url))
             ftp.quit()
             return True
 
+        log.info('THR_FTP: downloading...')
         for f in files:
             # The masterdata file
             if f[-1][:11] == 'export_pim_':
@@ -83,6 +95,7 @@ class product(osv.Model):
                     ftp.retrbinary('RETR {!s}'.format(f[-1]), out.write)
                     out.close()
                 except Exception as e:
+                    log.warning('THR_FTP: Tried to download file {!s} to {!s}, but got the following error: {!s}'.format(f[-1], path, str(e)))
                     helpdesk_db.create_simple_case(cr, uid, header, 'Tried to download file {!s} to {!s}, but got the following error: {!s}'.format(f[-1], path, str(e)))
                     return True
 
@@ -96,6 +109,7 @@ class product(osv.Model):
                     ftp.retrbinary('RETR {!s}'.format(f[-1]), out.write)
                     out.close()
                 except Exception as e:
+                    log.warning('THR_FTP: Tried to download file {!s} to {!s}, but got the following error: {!s}'.format(f[-1], path, str(e)))
                     helpdesk_db.create_simple_case(cr, uid, header, 'Tried to download file {!s} to {!s}, but got the following error: {!s}'.format(f[-1], path, str(e)))
                     return True
 
@@ -103,7 +117,8 @@ class product(osv.Model):
         # Close the connection and leave the program
         # ------------------------------------------
         ftp.quit()
-        exit()
+        log.info('THR_FTP: Process is complete.')
+        return True
 
 
 
