54bacc7442531dc9566e0acb6c25307ab8e3e901
Niels Ruelens <niels.ruelens@clubit.be>
2014-07-13 19:54:23 +0200

    added new EDI Settings functionality: Root Category selection for a partner

--- settings.py
+++ settings.py
@@ -1,6 +1,16 @@
 from openerp.osv import osv, fields
 from openerp.tools.translate import _
 
+
+class clubit_tools_settings_root_category(osv.Model):
+    _name = "clubit.tools.settings.root.category"
+    _columns = {
+        'setting': fields.many2one('clubit.tools.settings', 'Settings', ondelete='cascade', required=True, select=True),
+        'partner_id': fields.many2one('res.partner', 'Partner', ondelete='cascade', required=True, select=True),
+        'category_id': fields.many2one('product.category', 'Category', required=True, select=True),
+    }
+
+
 class clubit_tools_settings(osv.Model):
 
     _name = "clubit.tools.settings"
@@ -12,6 +22,7 @@ class clubit_tools_settings(osv.Model):
         'sale_low_tax_id': fields.many2one('account.tax', 'Sale Low Tax', required=True),
         'purchase_high_tax_id': fields.many2one('account.tax', 'Purchase High Tax', required=True),
         'purchase_low_tax_id': fields.many2one('account.tax', 'Purchase Low Tax', required=True),
+        'root_categories': fields.one2many('clubit.tools.settings.root.category', 'setting', 'Root Categories'),
     }
 
 
