626ca7c45b3ce02e2eb34b887e6874be95448aa8
Niels Ruelens <niels.ruelens@clubit.be>
2014-07-23 18:12:24 +0200

    added a small log message so we can trace when the mass save is done

--- wizard/product_save_all.py
+++ wizard/product_save_all.py
@@ -1,6 +1,6 @@
 import threading
 from openerp import pooler
-
+import logging
 from openerp.osv import osv
 
 class webdust_product_save_all(osv.TransientModel):
@@ -8,11 +8,14 @@ class webdust_product_save_all(osv.TransientModel):
     _description = 'Mass save every product in the DB'
 
     def _background(self, cr, uid, ids, context=None):
+        log = logging.getLogger(None)
+        log.info('MASS-PRODUCT-SAVE: saving every single product. This is gonna take a while')
         prod_db = self.pool.get('product.product')
         new_cr = pooler.get_db(cr.dbname).cursor()
         prod_db.write(new_cr, uid, prod_db.search(new_cr, uid, [], context=context), {}, context=context)
         new_cr.commit()
         new_cr.close()
+        log.info('MASS-PRODUCT-SAVE: mass save is complete.')
         return {}
 
     def start(self, cr, uid, ids, context=None):
