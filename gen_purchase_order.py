a882b7891bdb798788b567359b1f5ed58c25f16e
Niels Ruelens <niels.ruelens@clubit.be>
2014-08-08 21:23:05 +0200

    tweakers

--- gen_purchase_order.py
+++ gen_purchase_order.py
@@ -340,6 +340,7 @@ class purchase_order(osv.Model):
             if not case:
                 helpdesk_db.create_simple_case(cr, uid, 'Quotation {!s} has been open for longer than 15 minutes.'.format(order.name), error, 'purchase.order,{!s}'.format(str(order.id)))
             else:
+                case.write({'desciption': error})
                 helpdesk_db.case_reset(cr, uid, [case.id])
         return True
 
