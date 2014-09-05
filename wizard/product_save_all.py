9e1c3881f0b93549e1b042a594942236ca30ac60
Niels Ruelens <niels.ruelens@clubit.be>
2014-09-05 10:42:32 +0200

    added parameters to mass save

--- wizard/product_save_all.py
+++ wizard/product_save_all.py
@@ -6,9 +6,18 @@ from openerp.osv import osv, fields
 class webdust_product_save_all(osv.TransientModel):
     _name = 'webdust.product.save.all'
     _description = 'Mass save every product in the DB'
-    
+
     _columns = {
         'only_prices' : fields.boolean('Save pricing only'),
+        'offset' : fields.integer('Offset'),
+        'size' : fields.integer('Size'),
+        'page_size' : fields.integer('Page size')
+    }
+
+    _defaults = {
+        'offset' : 0,
+        'page_size' : 0,
+        'size': lambda self,cr,uid,c: len(self.pool.get('product.product').search(cr, uid, [])),
     }
 
     def _background(self, cr, uid, ids, context={}):
@@ -16,21 +25,19 @@ class webdust_product_save_all(osv.TransientModel):
         log.info('MASS-PRODUCT-SAVE: saving every single product. This is gonna take a while')
         prod_db = self.pool.get('product.product')
         new_cr = pooler.get_db(cr.dbname).cursor()
+        wizard = self.browse(new_cr, uid, ids[0])
 
         context['save_anyway'] = True
         products = prod_db.search(new_cr, uid, [], context=context)
-        
-        offset = 0
-        size = len(products)
-        page_size = 1000
-        offset = 10000
-        size = 50
-        page_size = 10
+
+        offset = wizard.offset
+        size = wizard.size
+        page_size = wizard.page_size
         for i in xrange(offset, size, page_size):
             log.info('MASS-PRODUCT-SAVE: saving products {!s} to {!s} of {!s}.'.format(i,i+page_size, size))
             # make sure product data is up-to-date
             prod_db.write(new_cr, uid, products[i:i+page_size], {}, context=context)
-            
+
         new_cr.commit()
         new_cr.close()
         log.info('MASS-PRODUCT-SAVE: mass save is complete.')
